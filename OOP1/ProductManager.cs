f6eb96ddcc016c1654da666b8a7957fc5a537ced
Hacik_Ulu <hacikd2@gmail.com>
2023-03-18 15:09:55 +0300

    Add project files.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün Eklendi:" + product.ProductName);
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine("Ürün Güncellendi:" + product.ProductName);
+
+        }
+
+        public void ListProduct(Product[] products)
+        {
+            foreach (Product product in products) 
+            {
+                Console.WriteLine("Ürün Id:"+product.Id);
+                Console.WriteLine("Kategori Id:"+product.CategoryId);
+                Console.WriteLine("Ürün Birim Fiyatı:"+product.UnitPrice);
+                Console.WriteLine("Ürün Adı:"+product.ProductName);
+                Console.WriteLine("Ürün Stok Adedi:"+product.UnitsInStock);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.Id = 1;
+            p1.CategoryId = 2;
+            p1.ProductName = "Kahve Makinesi";
+            p1.UnitPrice = 4500;
+            p1.UnitsInStock = 2;
+
+            Product p2 = new Product
+            {
+                Id = 2,
+                CategoryId = 3,
+                ProductName = "İphone 14 Pro Max",
+                UnitPrice = 50000,
+                UnitsInStock = 5
+            };
+
+            //PascalCase   camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(p1);
+            productManager.Add(p2);
+
+            //eklenen ürünlerin listelenmesi
+            Product[] products = new Product[] { p1, p2 };
+            Console.WriteLine();
+
+            Console.WriteLine("Eklenen Ürünlerin Listesi");
+            Console.WriteLine();
+
+            productManager.ListProduct(products);
+            Console.WriteLine("Uygulama sonlandırıldı!");
+            
+
+        }
+    }
+}
