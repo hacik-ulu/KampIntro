bb69b867a7bf453caf2d69916c41aab188b5a51f
Hacik_Ulu <hacikd2@gmail.com>
2023-03-19 17:07:54 +0300

    Yazılım Geliştirme Kampı Çalışmaları

---
+++ Generics/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyList<T> //Generic class
+    {
+        T[] cityArray; // gönderilen şehirlerin array olarak tutulması.
+        T[] tempArray;  // eski elemanların kaybolmasını engellemek için geçici array oluşturuyoruz.
+        public MyList()
+        {
+            cityArray = new T[0]; // MyList<string> cities2 = new MyList<string>(); işlem yapıldığında 0 elemanlı boş array oluşturulur.
+        }
+
+
+        public void Add(T item)
+        {
+            tempArray = cityArray; // geçici array ana arrayin elemanlarını tutuyor.
+            cityArray = new T[cityArray.Length + 1]; // eleman eklendikçe 1 artıyor.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                cityArray[i] = tempArray[i];
+            }
+
+            cityArray[cityArray.Length - 1] = item; // en son eklediğimiz itemi arrayın en son indexine dahil ediyoruz.
+
+        }
+
+        public int Count
+        {
+            get { return cityArray.Length; }
+
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hazır Koleksiyonun Eleman Sayısı");
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("İstanbul");
+            cities.Add("İzmir");
+            cities.Add("Bolu");
+            cities.Add("Sivas");
+            Console.WriteLine(cities.Count);
+
+            Console.WriteLine("Manuel Koleksiyonun Eleman Sayısı");
+            MyList<string> cities2 = new MyList<string>(); // <string> generic ifadesi için classı <T> şeklinde generic yaparız.
+            cities2.Add("Ankara");
+            cities2.Add("Elazığ");
+            cities2.Add("Muş");
+            cities2.Add("İzmir");
+            cities2.Add("Tunceli");
+            cities2.Add("Konya");
+            Console.WriteLine(cities2.Count);
+
+        }
+    }
+}
