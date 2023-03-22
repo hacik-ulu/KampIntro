9e3475f94d69b46d490294554e21644ade8be57b
Hacik_Ulu <hacikd2@gmail.com>
2023-03-22 20:52:22 +0300

    Yazılım Geliştirme Kampı Çalışmaları

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        //MicroService
+
+        //Yapı içinde yapı kullanılması
+        //class içinde başka bir class benzeri bir yapı kullanılırken newlemek yerine constructoru oluşturulur.
+
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            //girilen bilgiler uyuşuyor mu diye kontrol edilir.
+
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Oyuncu kaydedildi:" + gamer.FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama Başarısız.Kayıt yapılamadı.");
+            }
+
+
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Oyuncu Silindi:" + gamer.FirstName);
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Oyuncu Güncellendi:" + gamer.FirstName);
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthDate == 2002 && gamer.FirstName == "Haçik" 
+                && gamer.LastName == "Ulu" && gamer.IdentityNumber == 123456)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
